57e6b27f17f1ec6b90568a020f299cb54ad94924
mukil <mukila.kanibalan@frenzo.io>
2023-10-17 16:12:05 +0530

    styling a component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,24 @@
+import "./button.css";
+
+const Button = () => {
+  return (
+    <button
+      className="btn"
+      style={{
+        backgroundColor: "green",
+      }}
+    >
+      click me
+    </button>
+  );
+};
+export default Button;
+
+export const Text = () => {
+  return (
+    <>
+      <h1>welcome</h1>
+      <h2>welcome</h2>
+    </>
+  );
+};
